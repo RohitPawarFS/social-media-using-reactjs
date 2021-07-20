248a355ecba9bf903d91d4d1c37fa886ff4cd82f
RohitPawarFS <rohitpawarrmp495@gmail.com>
2021-07-20 15:33:15 +0530

    Added store and UI and fetching posts

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
-function App() {
-  return (
-    <p>App</p>
-  );
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { fetchPosts } from '../actions/posts';
+import { PostsList } from './';
+
+class App extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
+
+  render() {
+    const { posts } = this.props;
+    return (
+      <div>
+        <PostsList posts={posts} />
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    posts: state.posts,
+  };
+}
+export default connect(mapStateToProps)(App);
